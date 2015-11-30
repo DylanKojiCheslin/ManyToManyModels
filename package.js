a7f98766aeed544252139481aa18e171d4779281
Aiko Cheslin <lucidpaper@gmail.com>
2015-11-30 15:47:49 -0700

    added package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    name: "socialize:linkable-model",
+    name: "socialize:many-to-many-model",
     summary: "",
     version: "0.1.0",
     git: "https://github.com/DylanKojiCheslin/ManyToManyModels.git"
